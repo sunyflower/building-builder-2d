f97e51715483ab8000a2ec5c3d5bfede3f1fd2e4
pola <polaaleksandrowicz@gmail.com>
2023-01-05 20:41:14 +0100

    praca w block script

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockScript : MonoBehaviour
-{
+public class BlockScript : MonoBehaviour {
+
+   private float min_X = -1.5f, max_X = 1.5f;
+
+   private bool canMove;
+   private float move_Speed = 2f;
+
+   private Rigidbody2D myBody;
+
+   private bool gameOver;
+   private bool ignoreCollision;
+   private bool ignoreTrigger;
+
+
+   void Awake() {
+        myBody = GetComponent<Rigidbody2D>();
+   }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        canMove = true;
+
+        if(Random.Range(0, 2) > 0) {
+            move_Speed *= -1f;
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        MoveBlock();
+    }
+    
+    void MoveBlock() {
+        if(canMove) {
+
+            Vector3 temp = transform.position;
+
+            temp.x += move_Speed * Time.deltaTime;
+
+            if(temp.x > max_X) {
+                move_Speed *= -1f;
+
+
+            } else if (temp.x < min_X) {
+                move_Speed *= -1f;
+            }
+            
+            transform.position = temp;
+        }  
     }
 }
